cc3f3e9f0ac3cf34d4270f6c900669fb9722fb2b
k8mimura <109508681+k8mimura@users.noreply.github.com>
2022-08-29 10:27:10 +0900

    db

--- module/db.js
+++ module/db.js
@@ -2,7 +2,7 @@ const pg = require('pg');
 
 const pool = new pg.Pool({
     user: "postgres",
-    host: "127.0.0.1",
+    host: "172.17.0.8",
     database: "postgres",
     password: "postgres",
     port: 5432,
